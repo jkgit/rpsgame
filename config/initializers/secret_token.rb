b252f3624cbc8dae60434507fa4553048bc194d9
Jay <jaykraly@gmail.com>
2014-05-02 03:34:00 -0400

    fix error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rpsgame::Application.config.secret_token = 'b3b71df45b30248750c8d8766489d4cf9224199b61c701ea104eac8e01ddd1728dd490e6f7e3e4a1bf0933430b47e0dcbc09e0ca1f8c96afd21f936ac31ba79b'
